82d59322049c05a9d9ea9ba263649148d3c0d84b
joshrcook <joshua.cook@snapav.com>
2020-03-30 09:28:04 -0400

    working on banner stylese

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,19 @@
-module.exports = {
-    plugins: [
-        require('postcss-import'),
-        require('postcss-nested'),
-        require('tailwindcss'),
-        require('autoprefixer'),
-    ],
-}
+module.exports = ({ env, file }) => ({
+    plugins: {
+        'postcss-import': {},
+        'tailwindcss': {},
+        'postcss-nested': {},
+        'postcss-prefix-selector': env === production ? false : {
+            prefix: `.tui-${file.basename.replace('.postcss', '')}`,
+            transform: function (prefix, selector, prefixedSelector) {
+                if (selector === 'body') {
+                  return 'body' + prefix;
+                } else {
+                  return prefixedSelector;
+                }
+            },
+        },
+        'autoprefixer': {},
+        'cssnano': env === 'production' ? {} : false,
+    },
+});
